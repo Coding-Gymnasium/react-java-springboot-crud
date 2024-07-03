43484432f21acc9857298f5b9183298478f2f92a
Nico Rithner <nicorithner@gmail.com>
2024-07-03 14:29:53 -0600

    Add findByName method to BrandEntity

--- crud-app-example/src/main/java/com/example/repository/BrandEntityRepository.java
+++ crud-app-example/src/main/java/com/example/repository/BrandEntityRepository.java
@@ -3,5 +3,8 @@ package com.example.repository;
 import com.example.entity.BrandEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BrandEntityRepository extends JpaRepository<BrandEntity, Long> {
+    Optional<BrandEntity> findByName(String name);
 }
