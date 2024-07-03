d1b15a6b19df10d25d79ac03efec180cdac2aa23
Nico Rithner <nicorithner@gmail.com>
2024-07-03 12:55:19 -0600

    Add CarService

---
+++ crud-app-example/src/main/java/com/example/service/CarService.java
@@ -0,0 +1,17 @@
+package com.example.service;
+
+import com.example.dto.CarDTO;
+
+import java.util.List;
+
+public interface CarService {
+
+    void save(CarDTO carDTO);
+
+    List<CarDTO> findAll();
+
+    CarDTO findById(Long id);
+
+    void delete(Long id);
+
+}
