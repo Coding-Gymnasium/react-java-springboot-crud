8ef34a499ac2ff5f74a130652edd6416f7af08f1
Nico Rithner <nicorithner@gmail.com>
2024-07-03 13:21:12 -0600

    Configure mapper

--- crud-app-example/src/main/java/com/example/mapper/CarMapper.java
+++ crud-app-example/src/main/java/com/example/mapper/CarMapper.java
@@ -3,6 +3,7 @@ package com.example.mapper;
 import com.example.dto.CarDTO;
 import com.example.entity.CarEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 
@@ -11,7 +12,11 @@ import org.mapstruct.ReportingPolicy;
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public interface CarMapper {
+    @Mapping(target = "year", source = "yearOfManufacture")
+    @Mapping(target = "brand", source = "brand.name")
     CarDTO fromCarEntityToCarDTO(CarEntity carEntity);
 
+    @Mapping(target = "yearOfManufacture", source = "year")
+    @Mapping(target = "brand.nam", ignore = true)
     CarEntity fromCarDTOToCarEntity(CarDTO carDTO);
 }
