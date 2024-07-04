478d8e79c5ab866baaef9260e61e26492bc4359b
Nico Rithner <nicorithner@gmail.com>
2024-07-04 17:22:11 -0600

    Add controller advisor MethodArgumentNotValidException

--- crud-app-example/src/main/java/com/example/controller/ControllerAdvisor.java
+++ crud-app-example/src/main/java/com/example/controller/ControllerAdvisor.java
@@ -2,11 +2,14 @@ package com.example.controller;
 
 import com.example.dto.ErrorDTO;
 import com.example.exception.NotFoundException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ControllerAdvisor {
@@ -15,6 +18,16 @@ public class ControllerAdvisor {
     public ResponseEntity<ErrorDTO> handleNotFoundException(NotFoundException exception) {
         return ResponseEntity.badRequest().body(ErrorDTO.builder()
                 .message(exception.getMessage())
+                .time(LocalDateTime.now())
+                .build());
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception ) {
+        return ResponseEntity.badRequest().body(ErrorDTO.builder()
+                .message(exception.getFieldErrors().stream()
+                        .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                        .collect(Collectors.joining(",")))
                 .time(LocalDateTime.now())
                 .build());
     }
