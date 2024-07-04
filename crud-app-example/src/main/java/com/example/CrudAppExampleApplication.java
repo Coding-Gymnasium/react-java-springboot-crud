bac6b977d00903359ef944805e6bef93a2dd53b4
Nico Rithner <nicorithner@gmail.com>
2024-07-04 17:21:35 -0600

    Lint file

--- crud-app-example/src/main/java/com/example/CrudAppExampleApplication.java
+++ crud-app-example/src/main/java/com/example/CrudAppExampleApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CrudAppExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrudAppExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CrudAppExampleApplication.class, args);
+    }
 
 }
